3951cbc7a1799ab38f195f5e4ac6d510a393b5b2
noah <noah@qubitsec.com>
2020-08-11 21:42:29 +0900

    쿼리문 오류 수정

--- src/main/java/com/simplify/sample/db/controller/TestController.java
+++ src/main/java/com/simplify/sample/db/controller/TestController.java
@@ -3,11 +3,13 @@ package com.simplify.sample.db.controller;
 import com.simplify.sample.db.dto.Test;
 import com.simplify.sample.db.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@Controller
 public class TestController {
 
     @Autowired
--- src/main/java/com/simplify/sample/db/service/TestService.java
+++ src/main/java/com/simplify/sample/db/service/TestService.java
@@ -3,9 +3,11 @@ package com.simplify.sample.db.service;
 import com.simplify.sample.db.dto.Test;
 import com.simplify.sample.db.mapper.TestMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TestService {
 
     @Autowired
